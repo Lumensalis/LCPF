aa0ad60d4ad4ee27985fae714ac6c4d6fca8fa98
James Fowler <james@openseaconsulting.com>
2025-03-14 19:08:17 -0400

    added memory stats, cleanup

--- CircuitPyPartialImage/code.py
+++ CircuitPyPartialImage/code.py
@@ -3,11 +3,17 @@ main = MainManager()
 
 from adafruit_simplemath import constrain
 
+#############################################################################
+# ControlVariables are used to specify logical inputs which have their
+# values published to the WebUI and can be modified
+# by various control inputs (encoder movement, WebUI, ... )
+
 targetAngle = main.addControlVariable( "angle", "Servo Angle", min=20, max=160, kind="int" )
 targetColor = main.addControlVariable( "color", "Pixel Strip Color", kind="RGB", startingValue=(255,0,0) )
 
-caernarfon = main.addCaernarfon( "WemosS2Mini", neoPixelCount=9 )
-caernarfon.initServo(1)
+#############################################################################
+# Add a CaernarfonCastle
+caernarfon = main.addCaernarfon( neoPixelCount=9, servos=1 )
 
 #############################################################################
 # I2C devices 
@@ -17,17 +23,14 @@ qtr = main.adafruitFactory.createQTRotaryEncoder(caernarfon.i2c)
 def encoderChanged(delta): 
     targetAngle.move( delta )
     print( f"targetAngle now {targetAngle.value}")
-    
+
 qtr.onMove( encoderChanged )
 
 # add a WII Nunchuk
 nunchuk = main.adafruitFactory.createNunchuk(caernarfon.i2c)
 
 # add a display
-DISPLAY_WIDTH, DISPLAY_HEIGHT = 128, 32
-display = main.i2cFactory.addDisplay_SSD1306(DISPLAY_WIDTH, DISPLAY_HEIGHT, caernarfon.i2c)
-display.fill(0)
-display.show()
+display = main.i2cFactory.addDisplay_SSD1306(128, 32, caernarfon.i2c)
 
 def setDisplayPixel( state, location ):
     display.pixel( location[0], location[1], state )
@@ -49,27 +52,29 @@ def loop():
     caernarfon.pixels.show()
 
     # clear the pixel we set last loop
-    # setDisplayPixel( 0, displayTargetPixel )
-    display.fill(0)
-    
+    setDisplayPixel( 0, displayTargetPixel )
+
     # determine new location - wrapping based on cycle PLUS joystick position
-    displayTargetPixel[0] = int(main.cycle + nx*DISPLAY_WIDTH) % DISPLAY_WIDTH
-    displayTargetPixel[1] = int(main.cycle + ny*DISPLAY_HEIGHT) % DISPLAY_HEIGHT
+    displayTargetPixel[0] = int(main.cycle + nx*display.displayWidth) % display.displayWidth
+    displayTargetPixel[1] = int(main.cycle + ny*display.displayHeight) % display.displayHeight
     setDisplayPixel( 1, displayTargetPixel )
     
     # set LED on QTRotaryEncoder - targetColor changed via Web UI
     qtr.pixel.fill(targetColor.value)
     qtr.updateStemmaEncoder()
     
-    # determine servo angle
+    # determine servo angle - must stay within range to avoid exceptions
     s1angle = constrain( 
             targetAngle.value + ( (ny * 40) if nunchuk.buttons.C else 0),
             20, 160
         )
     
     caernarfon.servo1.angle = s1angle
+    
     if main.cycle % 10 == 0:
+        display.fill(0)
         display.text(f'{main.cycle} {s1angle}', 0, 0, 1 )
+        
     display.show()
 
 main.addTask( loop )
