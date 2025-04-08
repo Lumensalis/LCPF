f407790c29088a7f55d33d5e991dfb3c1d68e892
James Fowler <james@openseaconsulting.com>
2025-04-08 11:06:08 -0400

    remove fss.log from git

--- devOps/fileSaveSync.py
+++ devOps/fileSaveSync.py
@@ -5,18 +5,19 @@ targetDir = "F:\\lib"
 submoduleDir = 'lumensaliscplib'
 
 def syncFile():
-    with open('fss.log', 'a' ) as fssLog:
-        def writeLog( s:str ):
-            print( s, file = fssLog )
-            
-        filename = sys.argv[1]
-        if not filename.startswith(submoduleDir):
-            writeLog( f"skipping {filename}, not in {submoduleDir}" )
-            return
-        inLibFilename = filename[len(submoduleDir)+1:]
-        target = os.path.join( targetDir,inLibFilename )
-        writeLog( f"updating {filename} to {target}" )
-        shutil.copy( filename, target )
+    #with open('fss.log', 'a' ) as fssLog:
+    #    def writeLog( s:str ):
+    #        print( s, file = fssLog )
+    def writeLog( s:str ):
+        print( s )
+    filename = sys.argv[1]
+    if not filename.startswith(submoduleDir):
+        writeLog( f"skipping {filename}, not in {submoduleDir}" )
+        return
+    inLibFilename = filename[len(submoduleDir)+1:]
+    target = os.path.join( targetDir,inLibFilename )
+    writeLog( f"updating {filename} to {target}" )
+    shutil.copy( filename, target )
 
 
 if __name__ == "__main__":
