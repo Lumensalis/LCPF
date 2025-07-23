81f228e645c29c9a5c0166e4bf38352539331349
James Fowler <james@openseaconsulting.com>
2025-07-22 22:52:52 -0400

    more pylint / typings

--- CircuitPyDependencies/lib/stupidArtnet/__init__.py
+++ CircuitPyDependencies/lib/stupidArtnet/__init__.py
@@ -1,4 +1,4 @@
 """Facilitates library imports."""
 from stupidArtnet.StupidArtnetServer import StupidArtnetServer
 from stupidArtnet.ArtnetUtils import shift_this, put_in_range, make_address_mask
-from .StupidArtnet import StupidArtnet
+from stupidArtnet.StupidArtnet import StupidArtnet
---
+++ typings/rainbowio.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+
+# pylint: disable=unused-import,import-error, unused-argument
+
+from typing import Any
+
+def colorwheel( v:float|int )-> int: ...
